65419d582e82f2a6d6433251e1e0d1846287447f
Abel Martin <abel.martin@gmail.com>
2014-05-01 23:01:50 -0400

    Phase 1: Clean up '/'
    * Created a constant
    * Created Pokemon::caught_vs_free

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,40 +2,20 @@ enable :sessions
 use Rack::Flash
 
 get '/' do
-  #Searching
-  @captured_pokemon = Pokemon.where(caught: true).order(:type)
-  @free_pokemon = Pokemon.where(caught: false).order(:type)
-
-  if params[:type]
-    @captured_pokemon = @captured_pokemon.where(type: params[:type])
-    @free_pokemon = @free_pokemon.where(type: params[:type])
-  end
-
-  @captured_pokemon = @captured_pokemon.order(:type, :id)
-  @free_pokemon = @free_pokemon.order(:type, :id)
-
-  @types = %w(
-    bug dark dragon electric fairy fighting fire flying
-    ghost grass ground ice normal poison psychic rock
-    shadow steel unknown water
-  )
+  @types = Pokemon::TYPES
+  @captured_pokemon, @free_pokemon = Pokemon.caught_vs_free(params[:type])
 
   erb :index
 end
 
 post '/catch/:id' do
-  @types = %w(
-    bug dark dragon electric fairy fighting fire flying
-    ghost grass ground ice normal poison psychic rock
-    shadow steel unknown water
-  )
   #Validations: Allowed to catch pokemon that follow the following rules:
   # you don't already have it
   # you don't already have 2 pokemon of that type
   found_pokemon = Pokemon.where(id: params[:id]).first
   caught_count = Pokemon.where(type: found_pokemon.type, caught: true).count
 
-  if caught_count < 2 && @types.include?( found_pokemon.type )
+  if caught_count < 2 && Pokemon::TYPES.include?( found_pokemon.type )
     found_pokemon.caught = true
     found_pokemon.save
     NotificationService.tell_friends "I caught #{found_pokemon.name.upcase}!"
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,19 @@
 class Pokemon < ActiveRecord::Base
   self.inheritance_column = nil #Defending against STI
+
+  TYPES = %w(
+    bug dark dragon electric fairy fighting fire flying
+    ghost grass ground ice normal poison psychic rock
+    shadow steel unknown water
+  )
+
+  def self.caught_vs_free(type_filter)
+    result = self.order(:type, :id)
+
+    if type_filter.present?
+      result = result.where(type: type_filter)
+    end
+
+    result.all.partition{|pokemon| pokemon.caught}
+  end
 end
